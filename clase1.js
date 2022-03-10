ee9f3234e3a88181349b928e15b7d2fc51fef548
ciyucapa <cindycaceres134@gmail.com>
2022-03-10 17:08:21 -0500

    feat: add functions figuras

--- clase1.js
+++ clase1.js
@@ -1,1 +1,30 @@
-let numero = 1
+console.group("Cuadrado");
+
+const perimetroCuadrado = (lado) => lado * 4;
+const areaCuadrado = (lado) => lado * lado;
+
+console.groupEnd();
+
+console.group("Triangulo");
+const perimetroTriangulo = (lado1, lado2, base ) => {
+    return lado1 + lado2 + base;
+}
+
+const areaTriangulo = (base, altura) => {
+    return (base * altura) / 2;
+}
+console.groupEnd();
+
+console.group("Circulo");
+const diametroCirculo = (radio) => radio * 2;
+
+const PI = Math.PI;
+
+const perimetroCirculo = (radio) => {
+    const diametro = diametroCirculo(radio);
+    return  diametro * PI ;
+}
+
+const areaCirculo = (radio) => (radio * 2) * PI;
+
+console.groupEnd();
